5ea3aa658ca471e1e52541f81fdc26f45b9625a5
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-23 18:28:25 -0700

    created a database folder which contains the schema and seeds used in mysql that will create the burger table and add the burgers

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('DBLDBL', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Club', true);
+
