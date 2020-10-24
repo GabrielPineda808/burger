b62799ba7171ff40d6a7d7caa5836fe0b38bc906
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-23 22:13:37 -0700

    final psh to heroku

---
+++ public/borgar.js
@@ -0,0 +1,44 @@
+$(function() {
+  $(".change-burger").on("click", function(event) {
+    var id = $(this).data("id");
+    var newBurger = $(this).data("newBurger");
+
+    var newBurgerState = {
+      devoured: newBurger
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState
+    }).then(
+      function() {
+        console.log("changed burger to", newBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
+  
