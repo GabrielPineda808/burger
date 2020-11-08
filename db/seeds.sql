eb338f26c74a75c0c7ccad08d2071d6f178bc2e2
Gabriel Pineda <gabepineda6@gmail.com>
2020-11-07 23:24:48 -0800

    added styling to the site

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
-INSERT INTO burgers (name, devoured) VALUES ('DblDbl', true);
-INSERT INTO burgers (name, devoured) VALUES ('Vegan', false);
-
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bobbi');
+INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO cats (name) VALUES ('Owens');
